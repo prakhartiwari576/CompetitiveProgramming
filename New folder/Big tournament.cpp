abc458dd6e0aa9e78017438ee3d8bf48ea02d0a8
Naman Lalit <33170861+naman99lalit@users.noreply.github.com>
2018-08-27 10:28:44 +0530

    Create Big tournament.cpp

---
+++ New folder/Big tournament.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int main()
+{
+    ll t,n,i,j,count;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        scanf("%lld",&n);
+        printf("%lld\n",n-1);
+    }
+    return 0;
+}
+
